ff200e82589840716fea49a4f57b23998a4ee19d
Nico <lawani.gk@gmail.com>
2023-10-06 19:35:37 +0100

    Generate seed data and update schema

---
+++ prisma/seed.ts
@@ -0,0 +1,29 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+async function main() {
+  const user1 = await prisma.user.create({
+    data: {
+      email: 'example@example.com',
+      name: 'Example User',
+      username: 'exampleuser',
+      password: 'securepassword',
+      imageUrl: 'link-to-image.jpg',
+    }
+  })
+  const circle1 = await prisma.accountabilityCircle.create({
+    data: {
+      name: 'Circle 1'
+    }
+  })
+}
+
+main().catch( e=> {
+  console.error(e)
+  process.exit(1)
+})
+.
+finally(async() => {
+  await prisma.$disconnect()
+})
+

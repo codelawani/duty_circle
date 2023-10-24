880b1a515d083849a83ead81cf0e7b1698507530
Nico <lawani.gk@gmail.com>
2023-10-24 16:16:20 +0100

    chore: add user id to session object

---
+++ src/lib/types/next-auth.d.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user?: {
+      id?: string | null;
+      email?: string | null;
+      image?: string | null;
+      name?: string | null;
+    };
+    expires: string;
+  }
+}

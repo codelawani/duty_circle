0bb64f73f74d49d99cc9d88ef0e726d9ff5c18d4
Efezino Idisi <enduranceidisi13@gmail.com>
2023-10-26 09:42:31 +0100

    fix: show only public tasks in feed

--- src/components/homepage/feed-list.tsx
+++ src/components/homepage/feed-list.tsx
@@ -5,24 +5,29 @@ import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 import { Icons } from '../icons';
 import { getFeed } from '@/src/utils/task/fetch';
+import { useMemo } from 'react';
 
 export default function FeedList() {
   const searchParams = useSearchParams();
   const page = Number(searchParams.get('page') ?? 1);
+  const tag = searchParams.get('tag') ?? '';
   const { data } = useQuery({
     queryKey: ['feed', page],
     queryFn: () => getFeed(page),
     staleTime: 5 * 60 * 1000,
     refetchInterval: 5 * 60 * 1000,
   });
+  const publicFeed = useMemo(() => {
+    return data?.tasks.filter((task) => task.public);
+  }, [data?.tasks]);
 
-  if (data?.tasks === undefined) return <></>;
+  if (publicFeed === undefined) return <></>;
 
   return (
     <div>
       <div className='first-of-type:border-t grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
-        {data.tasks.length > 0 ? (
-          data.tasks.map((task) => <FeedItem key={task.id} {...task} />)
+        {publicFeed.length > 0 ? (
+          publicFeed.map((task) => <FeedItem key={task.id} {...task} />)
         ) : (
           <p className='text-center py-6 capitalize md:col-span-2 lg:col-span-3'>
             nothing yet...
@@ -54,8 +59,8 @@ export default function FeedList() {
               page: page + 1,
             },
           }}
-          className={`${data.tasks.length === 0 && 'invisible'} ${
-            page === data.totalPages ? 'invisible' : 'visible'
+          className={`${publicFeed.length < 20 && 'invisible'} ${
+            page === data?.totalPages ? 'invisible' : 'visible'
           } capitalize bg-primary px-5 py-1 rounded-md text-primary-foreground hover:bg-secondary hover:text-secondary-foreground transition-colors duration-100 flex items-center gap-2`}
         >
           next
